6a4dc3bbbf5c876aa2f7df98180735bf7b044c2a
Chen Gong <ChenG_abc@outlook.com>
2025-09-04 21:01:42 -0400

    feat: add dpgap

--- test_scripts.sh
+++ test_scripts.sh
@@ -6,3 +6,5 @@ python run.py setup.n_gpus_per_node=3 -m PrivImage -dn covidx_32 -e 10.0 -ed def
 python run.py setup.n_gpus_per_node=4 public_data.name=null model.ckpt=/p/fzv6enresearch/DPImageBench/exp/pdp-diffusion/conditional_pretrain_imagenet_32_1000cond/pretrain/checkpoints/final_checkpoint.pth -m PDP-Diffusion -dn covidx_32 -e 10.0 -ed default_pretraing
 
 CUDA_VISIBLE_DEVICES=1 python eval.py -dn cifar10_32 -ep /p/fzv6enresearch/gap/exp/lora-cifar10-eps10
+
+# ln -s /bigtemp/fzv6en/gap_data/exp exp
