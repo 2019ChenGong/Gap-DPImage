7945da4ffc528b52ca7f323efef0485b4bde2a89
Chen Gong <ChenG_abc@outlook.com>
2025-09-04 23:13:42 -0400

    feat: add dpgap

---
+++ metrics/dp_metrics.py
@@ -0,0 +1,18 @@
+class DPMetric(object):
+
+
+    def __init__(self):
+
+        pass
+
+    def sensitive_data_load(self, dataloader, config):
+
+        pass
+
+    def public_model_load(self, dataloader, config):
+
+        pass
+
+    def cal_metric(self):
+
+        pass
