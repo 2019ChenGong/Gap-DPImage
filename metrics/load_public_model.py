d9e5f0f6aaaf5e537287f8abf89f7a775a212f87
Chen Gong <ChenG_abc@outlook.com>
2025-09-07 14:58:53 -0400

    feat: model adding done

--- metrics/load_public_model.py
+++ metrics/load_public_model.py
@@ -2,8 +2,30 @@ import torch
 import random
 from torch.utils.data import random_split, TensorDataset, Dataset, DataLoader, ConcatDataset
 from data.stylegan3.dataset import ImageFolderDataset
+from diffusers import StableDiffusionPipeline
 
+import os
+
+os.environ['HF_HOME'] = '/bigtemp/fzv6en/diffuser_cache'
 
 def load_public_model(public_model):  
+
+    if public_model == 'stable-diffusion-2-1-base':
+        print(f"Loading model: {public_model}")
+        model_id = "stabilityai/stable-diffusion-2-1-base"
+        model = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+        model = model.to("cuda")
+        print(f"Loading done!")
+
+    elif public_model == 'stable-diffusion-v1-5':
+        print(f"Loading model: {public_model}")
+        model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
+        model = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+        model = model.to("cuda")
+        print(f"Loading done!")
+
+    else:
+        print(f"Error: '{public_model}' is not a valid public model.")
+        return
     
-    return
+    return model
